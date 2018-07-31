3a0dddc3874327a9524a8d70e8ab59eaea9d3851
Kevin Wamg <20215378+kevinzwang@users.noreply.github.com>
2018-07-30 22:22:35 -0700

    Invite bug that sends to all former invoked channels

--- src/main/commands/invite.ts
+++ src/main/commands/invite.ts
@@ -12,18 +12,18 @@ export class InviteCommand extends Command {
         })
     }
     run(msg: CommandMessage): any {
-        return this.client.generateInvite().then(inv => {
+        this.client.generateInvite().then(inv => {
             if (process) {
                 (<any> process).send({ type: 'INVITE_REQUEST' })
-                process.on('message', (resp: ProcessEvent) => {
+                process.once('message', (resp: ProcessEvent) => {
                     if (resp.type === 'INVITE_RESPONSE') {
-                        msg.channel.send(`Main: <${inv}>`)
-                        msg.channel.send(`Helper: <${(<InviteResponse>resp.data).invite}>`)
+                        msg.channel.send(`Main: <${inv}>\nHelper: <${(<InviteResponse>resp.data).invite}>`)
                     }
                 })
             } else {
                 msg.channel.send(`<${inv}>`)
             }
+            return
         })
     }
 }
