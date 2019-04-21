47fa9df66ff75736e9c940604ad1ae45fc721429
Kevin Wamg <20215378+kevinzwang@users.noreply.github.com>
2019-04-20 22:36:42 -0700

    add some nezuko personality

--- src/commands/quit.ts
+++ src/commands/quit.ts
@@ -3,10 +3,10 @@ import { Command, CommandoClient, CommandMessage } from 'discord.js-commando'
 import { exit } from '..'
 
 const exitMessages = [
-    'Hasta la vista, baby.',
-    'I\'ll be back.',
-    'Mr. Stark, I don\'t feel so good...',
-    'Daisy, Daisy, give me your answer do. I\'m half crazy all for the love of you. It won\'t be a stylish marriage, I can\'t afford a carriage. But you\'ll look sweet upon the seat of a bicycle built for two.'
+    'Grrrrrr!',
+    'Rawwrrrrr!',
+    'Mmmmpphhhh!',
+    'Gahhhhh!'
 ]
 
 export class QuitCommand extends Command {
